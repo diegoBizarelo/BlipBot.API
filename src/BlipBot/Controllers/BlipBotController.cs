a389c03f46eb78ae213cf040d5444e7141350227
diegoBizarelo <54692838+diegoBizarelo@users.noreply.github.com>
2021-10-30 20:06:44 -0300

    Adicionar arquivos de projeto.

---
+++ src/BlipBot/Controllers/BlipBotController.cs
@@ -0,0 +1,28 @@
+﻿using BlitBot.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlipBot.API.Controllers
+{
+    [Route("api/bitbot")]
+    public class BlipBotController : MainController
+    {
+        private readonly IRequestGitHubService _requestGitHubService;
+        public BlipBotController(INotificador notificador, 
+                                 IRequestGitHubService requestGitHubService) : base(notificador)
+        {
+            _requestGitHubService = requestGitHubService;
+        }
+
+        [HttpGet("obter-informacao")]
+        public async Task<IActionResult> ObterInformacao()
+        
+        {
+            var repositoriosObtidos = await _requestGitHubService.ObterRepositoriosDoGitHub();
+            return CustomResponse(repositoriosObtidos);
+        }
+    }
+}
